3b8eefea30f01a62a123af595f0363519845ad93
Gabriel <ggauto994@gmail.com>
2023-05-25 17:00:14 -0400

    Update SOAPHelperTest.php

--- test/SOAPHelperTest.php
+++ test/SOAPHelperTest.php
@@ -6,44 +6,19 @@ require '../vendor/autoload.php'; // Include the Composer autoloader
 
 /// Obtener el xml
 $xmlFile = $argv[1];
-$local_cert = $argv[2];
-$local_pk = $argv[3];
+
 
 // $url ="https://www.w3schools.com/xml/tempconvert.asmx?WSDL";
 $url = "https://sifen-test.set.gov.py/de/ws/consultas/consulta-ruc.wsdl?wsdl";
-$passphrase = "171222";
-
-if (!file_exists($xmlFile)) {
-    echo "El archivo  no existe\n";
-    return false;
-}
-
-if (!file_exists($local_cert)) {
-    echo "El archivo  no existe\n";
-    return false;
-}
-
-if (!file_exists($local_pk)) {
-    echo "El archivo  no existe\n";
-    return false;
-}
+$passphrase = "120108";
+$local_cert = "test\80121930-2.pem";
 
 $options = array(
     'soap_version' => SOAP_1_2,
-    'use' => SOAP_DOCUMENT,
-    'exceptions' => true,
-    'trace' => true,
-    'cache_wsdl' => WSDL_CACHE_NONE,
-    'stream_context' => stream_context_create(array(
-        'ssl' => array(
-            'verify_peer' => false,
-            'verify_peer_name' => false,
-            'allow_self_signed' => true,
-            'local_cert' => $local_cert,
-            'local_pk' => $local_pk,
-            'passphrase' => $passphrase,
-        ),
-    )),
+    'trace' => 1,
+    'exceptions' => 0,
+    'local_cert' => $local_cert,
+    'passphrase' => $passphrase,
 );
 
 // //////////////////////////////////////////////////SOAPCLIENT//////////////////////////////////////
