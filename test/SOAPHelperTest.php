eeb981892e30d87ee3218c9334bd477bb88c7ce3
Gabriel <ggauto994@gmail.com>
2023-05-26 10:19:33 -0400

    esto pasa a un nuevo error :D

--- test/SOAPHelperTest.php
+++ test/SOAPHelperTest.php
@@ -5,11 +5,10 @@ use Abiliomp\Pkuatia\Helpers\SOAPHelper;
 require '../vendor/autoload.php'; 
 $xmlFile = $argv[1];
 $wsdlUrl = "https://sifen-test.set.gov.py/de/ws/consultas/consulta-ruc.wsdl?wsdl";
- $pemFile = dirname(__FILE__) . '\80121930-2.pem';
+$pemFile = dirname(__FILE__) . '\80121930-2.pem';
 $passphrase = '171222';
 $action = "siConsRUC";
 $envelopedXML = SOAPHelper::soapEnvelop($xmlFile);
-
 $options = [
     'trace' => 1,
     'soap_version' => SOAP_1_2,
@@ -20,11 +19,15 @@ $options = [
         'ssl' => [
             'verify_peer' => false,
             'verify_peer_name' => false,
-            'allow_self_signed' => true,
         ]
     ])
 ];
-$client = new SoapClient($wsdlUrl, $options);
+try {
+    $client = new SoapClient($wsdlUrl, $options);
+} catch (Exception $e) {
+    echo $e->getMessage();
+    return false;
+}
 $response = $client->__doRequest($envelopedXML, $wsdlUrl, $action, SOAP_1_2);
 echo $response;
 ?>
