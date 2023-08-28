80914064a7ed9eac52cd608ee010964fcc31153c
Gabriel <ggauto994@gmail.com>
2023-08-28 13:11:55 -0400

    Update ConsultaDE.php

--- test/ConsultaDE.php
+++ test/ConsultaDE.php
@@ -12,19 +12,22 @@ $keyFile = '80121930-2.pem.key';
 $keyPassphrase = '171222';
 
 $config = new Config();
-$config->env = 'prod';
+$config->env = 'dev';
 $config->certificateFilePath = $certFile;
 $config->privateKeyFilePath = $keyFile;
 $config->privateKeyPassphrase = $keyPassphrase;
 
 $testCDC = '01800090853013001001802422023052910014652161';
-
+$requestDate = new DateTime('now', new DateTimeZone('America/Asuncion'));
+//cast to string to avoid DateTime serialization error
+$requestDate = (string) $requestDate->format('d/m/Y H:i:s');
 try {
     echo "Prueba de Consulta de DE\n";
     echo "Inicializando Sifen... ";
     Sifen::Init($config);
     echo "OK\n";
     echo "Consultando DE con CDC " . $testCDC . "...\n";
+    echo "Hora de consulta: " . $requestDate . "\n";
     $res = Sifen::ConsultarDE($testCDC);
     echo "Resultado: \n";
     var_dump($res->getRContDe()->getRDe());
