a0777dc8d7f2c10ab0e14dbc43026755c9895a2a
Gabriel <ggauto994@gmail.com>
2023-09-12 10:38:42 -0400

    Update ConsultaRUC.php

--- test/ConsultaRUC.php
+++ test/ConsultaRUC.php
@@ -18,6 +18,9 @@ $config->privateKeyFilePath = $keyFile;
 $config->privateKeyPassphrase = $keyPassphrase;
 
 $testRuc = '80057292';
+$requestDate = new DateTime('now', new DateTimeZone('America/Asuncion'));
+//cast to string to avoid DateTime serialization error
+$requestDate = (string) $requestDate->format('d/m/Y H:i:s');
 
 try {
     echo "Prueba de Consulta de RUC\n";
@@ -25,6 +28,7 @@ try {
     Sifen::Init($config);
     echo "OK\n";
     echo "Consultando RUC " . $testRuc . "...\n";
+    echo "Hora de consulta: " . $requestDate . "\n";
     $res = Sifen::ConsultarRUC($testRuc);
     echo "Resultado: \n";
     echo json_encode($res, JSON_PRETTY_PRINT);
