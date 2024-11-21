391dd137a7d7f36d798310e070d7b94f7cad548c
Abilio Mancuello Petters <abiliomp@gmail.com>
2024-11-21 09:59:51 -0300

    Update ItemValorado.php

--- src/Core/DocumentosElectronicos/Traits/ItemValorado.php
+++ src/Core/DocumentosElectronicos/Traits/ItemValorado.php
@@ -211,10 +211,10 @@ trait ItemValorado {
                 $gCamIVA->setDBasExe($totOpeItem);
             }
             else {
-                $baseGravada = bcdiv(bcmul($totOpeItem, bcdiv($proporcionGravadaIVA, '100', 10), 10),bcadd('1', bcdiv(strval($tasaIVA->value), 100, 10), 10), 10);                
+                $baseGravada = bcdiv(bcmul($totOpeItem, bcdiv($proporcionGravadaIVA, '100', 10), 10), bcadd('1', bcdiv(strval($tasaIVA->value), 100, 10), 10), 10);                
                 $gCamIVA->setDBasGravIVA(NumberFunctions::bcround($baseGravada, 8));
                 $gCamIVA->setDLiqIVAItem(NumberFunctions::bcround(bcmul($baseGravada, bcdiv(strval($tasaIVA->value), 100, 10), 10), 8));
-                $gCamIVA->setDBasExe(bcsub(bcsub($totOpeItem, $baseGravada, 8), $gCamIVA->getDLiqIVAItem(), 8));
+                $gCamIVA->setDBasExe(bcsub(bcsub($totOpeItem, $gCamIVA->getDBasGravIVA(), 8), $gCamIVA->getDLiqIVAItem(), 8));
             }
             $gCamItem->setGCamIVA($gCamIVA);
         }
