3176d18714bcab10248af10a769f98c06519399f
Abilio Mancuello Petters <abiliomp@gmail.com>
2025-01-06 17:07:07 -0300

    Update GResProcLote.php

--- src/Core/Fields/Response/Batch/GResProcLote.php
+++ src/Core/Fields/Response/Batch/GResProcLote.php
@@ -14,11 +14,11 @@ use IonysDev\Pkuatia\Core\Fields\Response\GResProc;
  */
 class GResProcLote
 {
-                              // Id - Longirud - Ocurrencia - Descripción
+  // Id - Longirud - Ocurrencia - Descripción
   public String    $id;       // CRSch050 - 44   - 1-1   - CDC del DE
   public String    $dEstRes;  // CRSch051 - 8-30 - 1-1   - Estado del resultado (Aprobado, Aprobado con Obs. o Rechazado)
   public int       $dProtAut; // CRSch052 - 10   - 0-1   - Número de transacción (se genera solo si el estado es Aprobado)
-  public GResProc  $gResProc; // CRSch053 - G    - 1-100 - Grupo Mensaje de Resultado
+  public array     $gResProc; // CRSch053 - G    - 1-100 - Grupo Mensaje de Resultado
 
   ///////////////////////////////////////////////////////////////////////
   // Setters
@@ -72,11 +72,11 @@ class GResProcLote
   /**
    * Establece el valor de gResProc
    *
-   * @param TgResProc $gResProc
+   * @param array $gResProc
    *
    * @return self
    */
-  public function setGResProc(GResProc $gResProc): self
+  public function setGResProc(array $gResProc): self
   {
     $this->gResProc = $gResProc;
 
@@ -120,9 +120,9 @@ class GResProcLote
   /**
    * Obtiene el valor de gResProc
    *
-   * @return GResProc
+   * @return array
    */
-  public function getGResProc(): GResProc
+  public function getGResProc(): array
   {
     return $this->gResProc;
   }
@@ -141,24 +141,27 @@ class GResProcLote
   public static function FromSifenResponseObject($object): GResProcLote
   {
     $res = new GResProcLote();
-    
-    if(isset($object->id)) $res->setId($object->id);
+
+    if (isset($object->id)) $res->setId($object->id);
     else throw new \Exception("[GResProcLote] Error al instanciar respuesta, falta parametro: id", 1);
 
-    if(isset($object->dEstRes)) $res->setDEstRes($object->dEstRes);
+    if (isset($object->dEstRes)) $res->setDEstRes($object->dEstRes);
     else throw new \Exception("[GResProcLote] Error al instanciar respuesta, falta parametro: dEstRes", 1);
 
-    if(isset($object->dProtAut)) $res->setDProtAut($object->dProtAut);
-    
-    if(isset($object->gResProc)) {
-      if(is_array($object->gResProc)){
+    if (isset($object->dProtAut)) $res->setDProtAut($object->dProtAut);
+
+    if (isset($object->gResProc)) {
+      $aux = array();
+      if (is_array($object->gResProc)) {
         foreach ($object->gResProc as $item) {
-          $res->setGResProc(GResProc::FromSifenResponseObject($item));
-        }
+          array_push($aux, GResProc::FromSifenResponseObject($item));
+        }        
       }
-      else $res->setGResProc(GResProc::FromSifenResponseObject($object->gResProc));
-    }
-    else throw new \Exception("[GResProcLote] Error al instanciar respuesta, falta parametro: gResProc", 1);
+      else {
+        array_push($aux, GResProc::FromSifenResponseObject($object->gResProc));
+      };
+      $res->setGResProc($aux);
+    } else throw new \Exception("[GResProcLote] Error al instanciar respuesta, falta parametro: gResProc", 1);
 
     return $res;
   }
